e32bebfc99d331985b616f30065cb8ebda7dcd86
Preetithapa26 <25bcab49@kristujayanti.com>
2026-02-11 00:58:15 +0530

    backend

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import sqlite3
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS contacts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        email TEXT,
+        message TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    data = request.json
+
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO contacts(name,email,message) VALUES(?,?,?)",
+        (data["name"], data["email"], data["message"])
+    )
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Saved successfully"})
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
